c9efc09fc674b95ea696f45867d4916900c0ec28
Daniil Timachov <danyatimachov@gmail.com>
2021-05-26 20:13:39 +0300

    Adding tests description

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from models.board_meta import BackgroundMeta
 from context_factory import ContextFactory
 from db_manager import DBManager
 
+___test = 1
+
 
 def test_decorator():
+    print("Testing decorator ...")
+
     b = Board()
     b.add_task("Wash the car.")
     print(b.to_dict())
@@ -15,15 +19,24 @@ def test_decorator():
         "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg")
     print(b.to_dict())
 
+    print("=" * 50)
+
 
 def test_factory():
+    print("Testing factory ...")
+
     cf = ContextFactory()
     ctx = cf.create_context("userboard")
 
+    print(type(ctx))
     print(dir(ctx))
+
+    print("=" * 50)
 
 
 def test_singleton_and_proxy_and_builder():
+    print("Testing singleton, proxy and builder ...")
+
     dbm = DBManager.get_instance()
     dbm.create_schema()
 
@@ -31,8 +44,12 @@ def test_singleton_and_proxy_and_builder():
     user = mb.create_model('User', 'UserModel', username="ghadd")
     print(user)
 
+    print("=" * 50)
+
 
 def test_strategy(username='anon'):
+    print("Testing strategy with `username = {}`...".format(username))
+
     dbm = DBManager.get_instance()
     dbm.create_schema()
 
@@ -51,8 +68,12 @@ def test_strategy(username='anon'):
 
     ctx.exec()
 
+    print("=" * 50)
+
 
 def test_memento():
+    print("Testing memento ...")
+
     dbm = DBManager.get_instance()
     dbm.create_schema()
 
@@ -86,8 +107,12 @@ def test_memento():
 
     print(board.tasks)
 
+    print("=" * 50)
+
 
 def test_iterator():
+    print("Testing iterator ...")
+
     dbm = DBManager.get_instance()
     dbm.create_schema()
 
@@ -98,15 +123,14 @@ def test_iterator():
     for task in board:
         print(task)
 
+    print("=" * 50)
+
 
 if __name__ == "__main__":
-    test_decorator()
     test_singleton_and_proxy_and_builder()
     test_factory()
-
+    test_decorator()
     test_strategy('anon')
     test_strategy('cool_username')
-
     test_memento()
-
     test_iterator()
